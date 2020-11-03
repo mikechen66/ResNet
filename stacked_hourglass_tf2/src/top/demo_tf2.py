eb77ea10b65200fa5afd791738d632467d3555f6
mikechen66 <875661606@qq.com>
2020-11-03 12:00:54 +0800

    Update demo_tf2.py

--- stacked_hourglass_tf2/src/top/demo_tf2.py
+++ stacked_hourglass_tf2/src/top/demo_tf2.py
@@ -4,9 +4,9 @@
 
 import sys
 
-sys.path.insert(0, "../data_gen/")
-sys.path.insert(0, "../net/")
-sys.path.insert(0, "../eval/")
+sys.path.insert(0, "/home/mike/Documents/keras_stacked_hourglass/src/data_gen/")
+sys.path.insert(0, "/home/mike/Documents/keras_stacked_hourglass/src/net/")
+sys.path.insert(0, "/home/mike/Documents/keras_stacked_hourglass/src/eval/")
 
 import os
 import numpy as np
